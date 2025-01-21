9f222497e893123ca0d020150c9e653e812bb272
tmay57 <tmay@inscico.com>
2025-01-21 14:16:35 -0800

    Initial Commit

---
+++ MauiApp1 Watch App/MauiApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  MauiApp1App.swift
+//  MauiApp1 Watch App
+//
+//  Created by Timothy May on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MauiApp1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MauiApp1/MauiApp1App.swift
@@ -0,0 +1,17 @@
+//
+//  MauiApp1App.swift
+//  MauiApp1
+//
+//  Created by Timothy May on 1/21/25.
+//
+
+import SwiftUI
+
+@main
+struct MauiApp1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
