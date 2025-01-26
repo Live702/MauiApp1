8b2b631ec4a9131faf2f8aabcc693aa8b3521063
tmay57 <tmay@inscico.com>
2025-01-26 11:47:47 -0800

    logs on iOS work

---
+++ DerivedData/MauiApp1/Build/Intermediates.noindex/swift-system.build/Debug-watchsimulator/CSystem.build/Objects-normal/i386/shims.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/timothymay/Library/Developer/Xcode/DerivedData/MauiApp1-giwbgscmfkvzjheapbmrwtwvnmxq/SourcePackages/checkouts/swift-system/Sources/CSystem/include/module.modulemap \
+  /Users/timothymay/Library/Developer/Xcode/DerivedData/MauiApp1-giwbgscmfkvzjheapbmrwtwvnmxq/SourcePackages/checkouts/swift-system/Sources/CSystem/shims.c
